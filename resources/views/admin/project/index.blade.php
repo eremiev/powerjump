1fc2a7f5f41fc398b6121797227bc0fb6dd6b401
Emil Eremiev <eeremiev@MacBook-Pro-de-usuario-2.local>
2018-07-24 16:20:38 +0200

    add create

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -4,7 +4,6 @@
     Проекти
 @endsection
 @section('container')
-
     <table class="table">
         <thead>
         <tr>
@@ -22,13 +21,29 @@
                 <td>{{$project->title}}</td>
                 <td>{{ str_limit($project->description, $limit = 100, $end = '...')}}</td>
                 <td>{{$project->when}}</td>
-                <td class="small">промени|изтрий</td>
+                <td class="small">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <a href="{{route('admin.projects.edit', $project->id)}}">
+                                <i class="fa fa-pencil" style="font-size:24px" aria-hidden="true"></i>
+                            </a>
+                        </div>
+                        <div class="col-lg-6">
+                            {{ Form::open(['route' => ['admin.projects.destroy', $project->id], 'method' => 'delete']) }}
+                            <button type="submit" style="padding: 0;border: none;background: none; cursor: pointer;">
+                                <i class="fa fa-trash" style="font-size:24px" aria-hidden="true"></i>
+                            </button>
+
+                            {{ Form::close() }}
+                        </div>
+                    </div>
+                </td>
             </tr>
         @endforeach
         </tbody>
     </table>
 
-        <div class="text-center">
-            {{ $projects->render() }}
-        </div>
+    <div class="text-center">
+        {{ $projects->render() }}
+    </div>
 @endsection
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,6 +21,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-bs4.css" rel="stylesheet">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote-bs4.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 <body>
